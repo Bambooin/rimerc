6c3b4c170a28bef00986521b119b74dcb6c372a1
Bambooin <BambooinLinux@163.com>
2021-04-01 14:16:51 +0800

    feat: add init script for squirrel

--- rimerc.sh
+++ rimerc.sh
@@ -6,6 +6,7 @@ if [ -z "$1" ]; then
   exit
 fi
 
+VALID_RIME="fcitx, fcitx5, ibus or squirrel"
 case $1 in
 fcitx)
   RIME_CONFIG_PATH=~/.config/fcitx/rime
@@ -18,9 +19,13 @@ fcitx5)
 ibus)
   RIME_CONFIG_PATH=~/.config/ibus/rime
   ;;
+
+squirrel)
+  RIME_CONFIG_PATH=~/Library/Rime
+  ;;
 *)
   echo "Invalid $1 input method"
-  echo "Valid input method is fcitx, fcitx5 or ibus"
+  echo "Valid input method is ${VALID_RIME}"
   exit
   ;;
 esac
@@ -31,11 +36,18 @@ EXTRACT=7z
 INPUT_METHOD=$1
 RIMERC=rimerc
 
-# check if fcitx installed
-if ! command -v ${INPUT_METHOD} &>/dev/null; then
-  echo "${INPUT_METHOD} could not be found"
-  echo "Please install ${INPUT_METHOD}-rime in your distro and try again!"
-  exit
+# check if input method installed
+if [ $1 == squirrel ]; then
+  if [ ! -d '/Library/Input Methods/Squirrel.app' ]; then
+    echo "Please install ${INPUT_METHOD} in your macOS and try again!"
+    exit
+  fi
+else
+  if ! command -v ${INPUT_METHOD} &>/dev/null; then
+    echo "${INPUT_METHOD} could not be found"
+    echo "Please install ${INPUT_METHOD} in your distro and try again!"
+    exit
+  fi
 fi
 
 # check if 7z installed
@@ -46,28 +58,27 @@ if ! command -v ${EXTRACT} &>/dev/null; then
 fi
 
 # extract
-cd ${RIMERC}
 TMP_DIR=$(mktemp -d -t ${RIMERC}-XXXX)
-echo -e "\nExtracting archive ${INPUT_METHOD}.7z to ${TMP_DIR} ..."
-7z e ${INPUT_METHOD}.7z -o${TMP_DIR} -primerc
+echo -e "Extracting archive ${INPUT_METHOD}.7z to ${TMP_DIR} ..."
+7z e ${INPUT_METHOD}.7z -o${TMP_DIR} -primerc >/dev/null 2>&1
 
 # backup rime config
 if [ -d ${RIME_CONFIG_PATH} ]; then
-  BACKUP_PATH=$(dirname ${RIME_CONFIG_PATH})/unrime
-  mv ${RIME_CONFIG_PATH} ${BACKUP_PATH}
-  echo -e "Backup your rime config to ${BACKUP_PATH}\n"
+  BACKUP_PATH=$(mktemp -p $(dirname ${RIME_CONFIG_PATH}) -d \
+    -t ${RIMERC}-backup-$(date '+%Y%m%d')-XXXX)
+  mv ${RIME_CONFIG_PATH}/* ${BACKUP_PATH}
+  echo -e "\nBackup your rime config to ${BACKUP_PATH}"
 fi
 
 # copy yaml to config path
-cd ${TMP_DIR}
 mkdir -p ${RIME_CONFIG_PATH}
-cp *.yaml ${RIME_CONFIG_PATH}
+cp ${TMP_DIR}/*.yaml ${RIME_CONFIG_PATH}
 
 # remove tmp dir
 rm -rf ${TMP_DIR}
 
 # restart input method
-echo "Waiting for ${INPUT_METHOD} restart and deploying..."
+echo -e "\nWaiting for ${INPUT_METHOD} restart and deploying..."
 case ${INPUT_METHOD} in
 fcitx | fcitx5)
   nohup ${INPUT_METHOD} -r >/dev/null 2>&1 &
@@ -76,9 +87,15 @@ fcitx | fcitx5)
 ibus)
   nohup ${INPUT_METHOD} restart >/dev/null 2>&1 &
   ;;
+
+squirrel)
+  RED='\033[0;31m'
+  CLEAR='\033[0m'
+  echo -e "${RED}Please deploy ${INPUT_METHOD} manually!${CLEAR}"
+  ;;
 *)
   echo "Invalid ${INPUT_METHOD} input method"
-  echo "Valid input method is ibus, fcitx or fcitx5"
+  echo "Valid input method is ${VALID_RIME}"
   exit
   ;;
 esac
