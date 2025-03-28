120414c0a37de843db1e4838185ca6130c0bc1e6
Bambooin <ooverflow@outlook.com>
2025-03-28 20:10:51 +0800

    chore: add change log to the release

--- script/archive.sh
+++ script/archive.sh
@@ -4,14 +4,14 @@ releasePath=release
 
 flavors=(fcitx fcitx5 ibus squirrel trime weasel)
 
-licence='Copyleft.txt LICENSE README.md'
+assets='Copyleft.txt LICENSE README.md CHANGELOG.md'
 
 version=$(git describe --tags --abbrev=0)
 
 pushd ${releasePath}
 
 for flavor in ${flavors[*]}; do
-  zip -r rimerc-${flavor}-${version}.zip ${flavor} ${licence} -x "*/.*"
+  zip -r rimerc-${flavor}-${version}.zip ${flavor} ${assets} -x "*/.*"
 done
 
 popd
